22f5383f44cab33177795f96cecabe4c0dad4b0a
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-19 08:34:02 +0600

    complete the mobile menu

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+    const sideMenuRef = useRef();
+
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
   return (
    <>
    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -38,12 +47,33 @@ const Navbar = () => {
                 <Image className='w-6' src={assets.moon_icon} alt='not fount'/>
             </button>
             <Link className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 fontOvo capitalize' href='#contact'>contact <Image className='w-3' alt='not fount' src={assets.arrow_icon}/></Link>
-            <button className='block md:hidden ml-3'>
+            <button className='block md:hidden ml-3' onClick={openMenu}>
                 <Image className='w-6' src={assets.menu_black} alt='not fount'/>
             </button>
         </div>
 
         {/* ------------- Mobail menu ----------*/}
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 px-20 py-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+            <div className='absolute right-6 top-6' onClick={closeMenu}>
+                <Image className='w-6 cursor-pointer' src={assets.close_black} alt = 'not fount'/>
+            </div>
+
+            <li>
+                <Link className='fontOvo capitalize' href='#top' onClick={closeMenu}>home</Link>
+            </li>
+            <li>
+               <Link className='fontOvo' href='#about' onClick={closeMenu}>About me</Link>
+            </li>
+            <li>
+                 <Link className='fontOvo capitalize' href='#services' onClick={closeMenu}>services</Link>
+            </li>
+            <li>
+                <Link className='fontOvo capitalize' href='#work' onClick={closeMenu}>my work</Link>
+            </li>
+            <li>
+                <Link className='fontOvo' href='#contact' onClick={closeMenu}>Contact me</Link>
+            </li>
+        </ul>
     </nav>
    </>
   )
