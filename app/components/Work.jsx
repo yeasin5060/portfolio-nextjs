d5a3624ae4ff38381a91a0f301f7bb1e12f75c24
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-20 09:25:30 +0600

    complete the animation in work section

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,18 +2,42 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Work = () => {
   return (
-    <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg fontOvo capitalize'>my portfolio</h4>
-        <h2 className='text-center text-5xl fontOvo capitalize'>my letest works</h2>
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 fontOvo'>Welcome to my web development portfolio. I specialize in building full-stack web applications using modern technologies like the MERN stack, with a strong focus on performance, scalability, and user experience. I’m passionate about turning complex problems into simple, elegant solutions.</p>
+    <motion.div
+        initial = {{opacity : 0}}
+        whileInView={{opacity : 1}}
+        transition={{duration : 1}}
+        id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <motion.h4
+            initial = {{y : -20 , opacity : 0}}
+            whileInView={{y : 0 , opacity : 1}}
+            transition={{duration : 0.3 , delay : 0.6}}
+            className='text-center mb-2 text-lg fontOvo capitalize'>my portfolio</motion.h4>
+        <motion.h2
+            initial = {{y : -25 , opacity : 0}}
+            whileInView={{y : 0 , opacity : 1}}
+            transition={{duration : 0.5 , delay : 0.6}}
+            className='text-center text-5xl fontOvo capitalize'>my letest works</motion.h2>
+        <motion.p
+            initial = {{opacity : 0}}
+            whileInView={{opacity : 1}}
+            transition={{duration : 0.7 , delay : 0.5}}
+            className='text-center max-w-2xl mx-auto mt-5 mb-12 fontOvo'>Welcome to my web development portfolio. I specialize in building full-stack web applications using modern technologies like the MERN stack, with a strong focus on performance, scalability, and user experience. I’m passionate about turning complex problems into simple, elegant solutions.</motion.p>
 
-        <div className='grid grid-cols-4 my-10 gap-5'>
+        <motion.div
+            initial = {{opacity : 0}}
+            whileInView={{opacity : 1}}
+            transition={{duration : 0.9 , delay : 0.6}}
+            className='grid grid-cols-4 my-10 gap-5'>
             {
                 workData.map((project , index)=> (
-                    <div className='aspect-square bg-no-repeat bg-center bg-cover rounded-lg relative cursor-pointer group' key={index} style = {{backgroundImage : `url(${project.bgImage})`}}>
+                    <motion.div
+                        whileHover={{scale : 1.05}}
+                        transition={{duration : 0.3 }}
+                        className='aspect-square bg-no-repeat bg-center bg-cover rounded-lg relative cursor-pointer group' key={index} style = {{backgroundImage : `url(${project.bgImage})`}}>
                         <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                             <div>
                                 <h2 className='font-semibold'>{project.title}</h2>
@@ -23,14 +47,18 @@ const Work = () => {
                                 <Image className='w-5' src={assets.send_icon} alt='not fount'/>
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 ))
             }
-        </div>
-        <Link className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 my-20 mx-auto hover:bg-blue-100 duration-500' href=''>
+        </motion.div>
+        <motion.a
+            initial = {{opacity : 0}}
+            whileInView={{opacity : 1}}
+            transition={{duration : 1.1 , delay : 0.6}}
+            className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 my-20 mx-auto hover:bg-blue-100 duration-500' href=''>
             Show more <Image className='w-4' src={assets.right_arrow_bold} alt='not fount'/>
-        </Link>
-    </div>
+        </motion.a>
+    </motion.div>
   )
 }
 
